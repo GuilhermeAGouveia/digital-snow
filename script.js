93faddf76d6d9a27be96295ec25aa4b17d16304f
GuilhermeAGouveia <guilhermegouveia.dev@gmail.com>
2022-06-01 15:09:29 -0300

    Code refactor

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var vet = [];
 var container = document.getElementById("elementsContainer")
 document.addEventListener("mousemove", e => {
   var elements = document.getElementsByClassName("neve")
-  for (let i = 0; i < elements.length; i++){
-    elements[i].style.left = (vet[i] + e.clientX  - window.outerWidth/2) + "px";
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.left = (vet[i] + e.clientX - window.outerWidth / 2) + "px";
   }
 })
 document.addEventListener("touchstart", e => {
@@ -13,70 +13,69 @@ document.addEventListener("touchstart", e => {
 document.addEventListener("touchmove", e => {
   var value = -1 * (toqueinit - e.targetTouches[0].pageX);
   var elements = document.getElementsByClassName("neve")
-  for (let i = 0; i < elements.length; i++){
+  for (let i = 0; i < elements.length; i++) {
     elements[i].style.left = (vet[i] + value) + "px";
   }
 
 })
-var o = 0;
-var s = 0;
-for (let i = 0; i < 50; i++){
 
-    var node = document.createElement("span");
-    node.setAttribute("class", "neve");
-    var x =  Math.floor(Math.random()  * 4 * window.outerWidth - window.outerWidth);
-    vet.push(x);
-    node.setAttribute("style", "left:" + x + "px; top: -8px;");
-    container.appendChild(node);
-    document.getElementsByClassName("neve")[i]
+for (let i = 0; i < 50; i++) {
+
+  var node = document.createElement("span");
+  node.setAttribute("class", "neve");
+  var x = Math.floor(Math.random() * 4 * window.outerWidth - window.outerWidth);
+  vet.push(x);
+  node.setAttribute("style", "left:" + x + "px; top: -8px;");
+  container.appendChild(node);
+  document.getElementsByClassName("neve")[i]
     .animate(
       [
-        {left: x + "px"},
-        {top: "100%"}
+        { left: x + "px" },
+        { top: "100%" }
       ],
       {
         duration: 5000,
         iterations: Infinity,
-        delay: Math.floor(Math.random()  * 100 * i)
+        delay: Math.floor(Math.random() * 100 * i)
       })
 
-    }
+}
 
-    for (let i = 0; i < 20; i++){
+for (let i = 0; i < 20; i++) {
 
-        var node = document.createElement("img");
-        node.setAttribute("class", "flock");
-        node.setAttribute("src", "./flock.svg");
-        var x =  Math.floor(Math.random()  * 2600);
+  var node = document.createElement("img");
+  node.setAttribute("class", "flock");
+  node.setAttribute("src", "./flock.svg");
+  var x = Math.floor(Math.random() * 2600);
 
-        node.setAttribute("style", "left:" + x + "px; top: -27px;");
-        container.appendChild(node);
-        document.getElementsByClassName("flock")[i]
-        .animate(
-          [
-            {left: x + "px"},
-            {top: "100%"},
+  node.setAttribute("style", "left:" + x + "px; top: -27px;");
+  container.appendChild(node);
+  document.getElementsByClassName("flock")[i]
+    .animate(
+      [
+        { left: x + "px" },
+        { top: "100%" },
 
 
-          ],
-          {
-            duration: 8000,
-            iterations: Infinity,
-            delay: Math.floor(Math.random()  * 10000),
+      ],
+      {
+        duration: 8000,
+        iterations: Infinity,
+        delay: Math.floor(Math.random() * 10000),
 
-          })
-          var dur = Math.floor(Math.random()  * 10000);
-          dur = dur < 8000 ? dur + 2000 : dur;
-          document.getElementsByClassName("flock")[i]
-          .animate(
-            [
-              {transform:"rotate(360deg)"}
-            ],
-            {
-              duration:dur,
-              iterations: Infinity,
-              easing: "linear"
+      })
+  var dur = Math.floor(Math.random() * 10000);
+  dur = dur < 8000 ? dur + 2000 : dur;
+  document.getElementsByClassName("flock")[i]
+    .animate(
+      [
+        { transform: "rotate(360deg)" }
+      ],
+      {
+        duration: dur,
+        iterations: Infinity,
+        easing: "linear"
 
-            })
+      })
 
-        }
+}
